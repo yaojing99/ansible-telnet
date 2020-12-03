a57c20b008ede799dd44043364989ff46e732578
Dean Zhu - 374724 <dean.r.zhu@rrd.com>
2020-12-03 14:24:55 +0800

    Update telnet_switch.sh

--- telnet_switch.sh
+++ telnet_switch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook -i /root/ansible_switch/hosts telnet_shapmt-sw-cr02.yml
-ansible-playbook -i /root/ansible_switch/hosts telnet_shachpmt-sw-cr25.yml
+ansible-playbook -i /root/ansible_switch/hosts /root/ansible_switch/telnet_shapmt-sw-cr02.yml
+ansible-playbook -i /root/ansible_switch/hosts /root/ansible_switch/telnet_shachpmt-sw-cr25.yml
