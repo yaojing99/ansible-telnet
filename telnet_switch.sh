45d674df7e56ac6287836bcee8ae87a88ab9283b
Dean Zhu - 374724 <dean.r.zhu@rrd.com>
2020-12-04 13:48:01 +0800

    Update vault

--- telnet_switch.sh
+++ telnet_switch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ansible-playbook -i hosts telnet_shapmt-sw-cr02.yml &&
-ansible-playbook -i hosts telnet_shachpmt-sw-cr25.yml &&
+ansible-playbook -i hosts --vault-password-file /root/ansibleCRED/vault-password-file telnet_shapmt-sw-cr02.yml &&
+ansible-playbook -i hosts --vault-password-file /root/ansibleCRED/vault-password-file telnet_shachpmt-sw-cr25.yml &&
 echo Completed!
